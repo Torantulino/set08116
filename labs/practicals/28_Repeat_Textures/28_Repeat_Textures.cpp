f453146305a70091e72de8b7bfd7482504f1848a
Torantulino <40276179@live.napier.ac.uk>
2018-02-10 19:53:16 +0000

    28 now working perfectly.

--- labs/practicals/28_Repeat_Textures/28_Repeat_Textures.cpp
+++ labs/practicals/28_Repeat_Textures/28_Repeat_Textures.cpp
@@ -14,11 +14,11 @@ texture tex;
 texture tex2;
 
 bool load_content() {
-  // Construct geometry object
+  // Construct geometry objects
   geometry geom;
   geometry geom1;
-  // Create triangle data
-  // Positions
+
+  // --Positions--
   //Right Quad
   vector<vec3> positions{	vec3(0.0f, 1.0f, 0.0f), vec3(0.0f, 0.0f, 0.0f), vec3(1.0f, 0.0f, 0.0f), 
 							vec3(1.0f, 1.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f), vec3(1.0f, 0.0f, 0.0f)
@@ -27,7 +27,8 @@ bool load_content() {
   vector<vec3> positions1{ vec3(-1.0f, 0.0f, 0.0f), vec3(-1.0f, 1.0f, 0.0f), vec3(-2.0f, 1.0f, 0.0f),
 							vec3(-2.0f, 0.0f, 0.0f), vec3(-1.0f, 0.0f, 0.0f), vec3(-2.0f, 1.0f, 0.0f)
   };
-  //Texture Coords
+
+  //--Texture Coords--
   //Right Quad
   vector<vec2> tex_coords{	vec2(0.0f, 1.0f), vec2(0.0f, 0.0f),vec2(1.0f, 0.0f), 
 							vec2(1.0f, 1.0f), vec2(0.0f, 1.0f), vec2(1.0f, 0.0f)
@@ -36,14 +37,16 @@ bool load_content() {
   vector<vec2> tex_coords1{ vec2(-1.0f, 0.0f), vec2(-1.0f, 1.0f), vec2(-2.0f, 1.0f),
 							vec2(-2.0f, 0.0f), vec2(-1.0f, 0.0f), vec2(-2.0f, 1.0f)
   };
+
   // Add to the geometrys
   geom.add_buffer(positions, BUFFER_INDEXES::POSITION_BUFFER);
   geom1.add_buffer(positions1, BUFFER_INDEXES::POSITION_BUFFER);
+
   // Add texture coordinate buffer to geometrys
   geom.add_buffer(tex_coords, BUFFER_INDEXES::TEXTURE_COORDS_0);
-  geom1.add_buffer(tex_coords1, BUFFER_INDEXES::TEXTURE_COORDS_1);
+  geom1.add_buffer(tex_coords1, BUFFER_INDEXES::TEXTURE_COORDS_0);
 
-  // Create mesh object
+  // Create mesh objects
   m = mesh(geom);
   m1 = mesh(geom1);
 
@@ -51,19 +54,21 @@ bool load_content() {
   eff.add_shader("27_Texturing_Shader/simple_texture.vert", GL_VERTEX_SHADER);
   eff.add_shader("27_Texturing_Shader/simple_texture.frag", GL_FRAGMENT_SHADER);
 
-  // *********************************
   // Build effect
   eff.build();
   // Load texture "textures/sign.jpg"
   tex = texture("textures/sign.jpg", true, false);
   tex2 = texture("textures/sign.jpg", false, false);
-  // *********************************
 
   // Set camera properties
   cam.set_position(vec3(-0.5f, 0.5f, 2.5f));
   cam.set_target(vec3(-0.5f, 0.5f, 0.0f));
   auto aspect = static_cast<float>(renderer::get_screen_width()) / static_cast<float>(renderer::get_screen_height());
   cam.set_projection(quarter_pi<float>(), aspect, 1.0f, 1000.0f);
+
+  //Move Meshes closer together
+  m.get_transform().position -= vec3(0.5f, vec2(0.0f));
+  m1.get_transform().position += vec3(0.5, vec2(0.0f));
 
   return true;
 }
@@ -77,42 +82,49 @@ bool update(float delta_time) {
 bool render() {
   // Bind effect
   renderer::bind(eff);
+  //------------------QUAD 1---------------
   // Create MVP matrix
   auto M = m.get_transform().get_transform_matrix();
   auto V = cam.get_view();
   auto P = cam.get_projection();
   auto MVP = P * V * M;
+
   // Set MVP matrix uniform
   glUniformMatrix4fv(eff.get_uniform_location("MVP"), // Location of uniform
                      1,                               // Number of values - 1 mat4
                      GL_FALSE,                        // Transpose the matrix?
                      value_ptr(MVP));                 // Pointer to matrix data
 
+
   // Bind texture to renderer
   renderer::bind(tex, 0);
-  renderer::bind(tex2, 0);
+
   // Set the texture value for the shader here
   glUniform1i(eff.get_uniform_location("tex"), 0);
-  glUniform1i(eff.get_uniform_location("tex2"), 1);
 
   // Render the mesh
   renderer::render(m);
 
-  //----------
-  auto M1 = m1.get_transform().get_transform_matrix();
+  //------------------QUAD 2---------------
+  // Create MVP matrix
+  M = m1.get_transform().get_transform_matrix();
   V = cam.get_view();
   P = cam.get_projection();
   MVP = P * V * M;
+
   // Set MVP matrix uniform
   glUniformMatrix4fv(eff.get_uniform_location("MVP"), // Location of uniform
 	  1,                               // Number of values - 1 mat4
 	  GL_FALSE,                        // Transpose the matrix?
 	  value_ptr(MVP));                 // Pointer to matrix data
 
-  //----------
+  renderer::bind(tex2, 0);
 
-  // Render the mesh
+  // Render second the mesh
   renderer::render(m1);
+
+  // Set the texture value for the shader here
+  glUniform1i(eff.get_uniform_location("tex"), 0);
 
   return true;
 }
